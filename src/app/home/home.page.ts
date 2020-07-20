933e50cffc578f526bb2b43d22eff1b91332af0d
Chandima Samarakoon <chandima_s@3logixs.lk>
2020-07-20 17:24:44 +0530

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,32 +7,13 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
   successMessage: String = '';
   loginForm:FormGroup;
 
-
-  // error_messages = {
-  //   'email':[
-  //     { type: 'minlength', message: 'Email must be at least 5 characters long.' },
-  //   ],
-  // }
- 
-//   'password':[
-// 		{ type: 'minlength', message: 'Password must be at least 5 characters long.' },
-// 		{ type: 'maxlength', message: 'Password cannot be more than 25 characters long.' },
-// 		//{ type: 'pattern', message: 'Your Password must contain only numbers and letters.' },
-    
-//   ],
-// }
-
-
-
-
-
 constructor(public formBuilder:FormBuilder,private router: Router) {
-
-this.loginForm = this.formBuilder.group({
+  this.loginForm = this.formBuilder.group({
   username: new FormControl(null, Validators.required),
   password: new FormControl(null, Validators.required)
 });
@@ -40,27 +21,18 @@ this.loginForm = this.formBuilder.group({
 
  isValid(controlName) {
   return this.loginForm.get(controlName).invalid && this.loginForm.get(controlName).touched;
-}
+  }
 
 
   login()
   {
     console.log('username:',this.loginForm.value.username);
     console.log('password:',this.loginForm.value.password);
-
     if(this.loginForm.value.username=="chandima" && this.loginForm.value.password=="1234"){
-    this.router.navigate(['todo-list']);
-    }
+    this.router.navigate(['todo-list']);  }
     else{
-
-      this.successMessage = 'Wrong User Name or Password';
-      console.log("error username and password")
-      
+      this.successMessage = 'Wrong Username or Password';
+      console.log("error username and password");
     }
   }
-
- 
-
-
-
 }
--- src/app/todo-list/todo-list.page.ts
+++ src/app/todo-list/todo-list.page.ts
@@ -1,19 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-
 import { Router, ActivatedRoute } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.page.html',
   styleUrls: ['./todo-list.page.scss'],
-  
 })
 export class TodoListPage implements OnInit {
-
-
 
   checkBoolean=false;
   value ='';
@@ -27,24 +21,18 @@ export class TodoListPage implements OnInit {
     }
   }
 
-additem(){
-let obj ={
-  value: this.value,
-  isDone: false
-}
+  additem(){
+    let obj ={
+    value: this.value,
+    isDone: false
+  }
+
 this.items.unshift(obj)
 localStorage.setItem('items', JSON.stringify(this.items))
 this.value=''
-  }
-
-// deleteitem(ind){
-//   this.items =this.items.filter((c, index)=> index !=ind)
-//   localStorage.setItem('items', JSON.stringify(this.items))
- 
-// }
+}
 
 setDone(index){
- 
 let item =this.items.find((c, ind) =>ind ==index)
 item.isDone =!item.isDone;
 this.checkBoolean=true;
@@ -52,7 +40,6 @@ localStorage.setItem('checkBoolean', JSON.stringify(true))
 console.log(this.checkBoolean);
 localStorage.setItem('items', JSON.stringify(this.items))
 }
-
 
 async presentAlertConfirm(ind) {
   const alert = await this.alertController.create({
@@ -71,16 +58,14 @@ async presentAlertConfirm(ind) {
         text: 'Okay',
         handler: () => {
           this.items =this.items.filter((c, index)=> index !=ind)
-  localStorage.setItem('items', JSON.stringify(this.items))
+          localStorage.setItem('items', JSON.stringify(this.items))
           console.log('Confirm Okay');
         }
       }
     ]
   });
-
   await alert.present();
 }
-
 
 onBooleanChange(){
   console.log(this.checkBoolean);
@@ -88,7 +73,5 @@ onBooleanChange(){
 
 logout(){
   this.router.navigate(['home']);
-
 }
-
 }
